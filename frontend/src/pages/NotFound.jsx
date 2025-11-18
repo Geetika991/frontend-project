fcbebf1222f4dd5d52a74ca32c7a9d1477f7f63f
Geetika <geetika.sharma1303@gmail.com>
2025-11-18 20:35:07 +0530

    added 404 not found page

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const NotFound = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
+      <h1 className="text-9xl font-extrabold text-white tracking-widest">
+        404
+      </h1>
+      <div className="bg-black text-white px-2 text-sm rounded-12  absolute">
+        Page not found
+      </div>
+      <button className="mt-5">
+        <a className="relative inline-block text-sm font-medium text-[#ff6a3d] group active:text-yellow-500 focus:outline-none focus:ring">
+          <span className="relative block px-8 py-3 bg-[#1a2238] border border-current" onClick={() => navigate(-1)}>
+            {" "}
+            Go Back
+          </span>
+        </a>
+      </button>
+    </div>
+  );
+};
+
+export default NotFound;
