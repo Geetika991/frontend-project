d18a2345dddc2d3cc92999bc7a733add68f4d773
Geetika <geetika.sharma1303@gmail.com>
2025-11-18 17:41:19 +0530

    setupof reduc toolkit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,16 @@
-import { BrowserRouter } from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Toaster from "react-hot-toast"
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {Toaster} from "react-hot-toast";
+import {Provider} from "react-redux";
+import store from "./Redux/Store/store.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-    <Toaster/>
+    <Provider store={store}>
+      <App />
+      <Toaster />
+    </Provider>
   </BrowserRouter>
 );
